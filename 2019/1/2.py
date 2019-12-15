1b4e59b4bc5229e2800b48cd9b62a13d3f5bcecf
root <root@localhost.localdomain>
2019-12-15 01:11:20 -0500

    fixed stuff

--- 2019/1/2.py
+++ 2019/1/2.py
@@ -4,20 +4,20 @@ file = open("input.txt","r")
 
 sum = 0
 fuel = 0
+exFuel = 0
 
+#have to add extra fuel for each module, then add total fuel for all modules at the end
 for i in file:
     i = i.rstrip()
     if i:
-        sum += math.trunc((int(i)/3)) - 2
+        print(i)
+        fuel = math.trunc((int(i)/3)) - 2
+        exFuel = fuel
+        while fuel > 0:
+            fuel = math.trunc((int(fuel)/3)) - 2
+            if fuel > 0:
+                print(fuel)
+                exFuel += fuel
+        sum += exFuel
 
-fuel = sum
-exFuel = 0
-
-while fuel > 0:
-    print(fuel)
-    fuel = math.trunc((int(fuel)/3)) - 2
-    exFuel += fuel
-
-exFuel += sum
-
-print(exFuel)
+print(sum)
